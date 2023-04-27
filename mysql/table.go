7b50245dc339a71724e8cf202895515e42db650a
hiroshi.nosaka <hiroshi.nosaka@sysadminnoMacBook-Pro.local>
2023-04-27 10:28:02 +0900

    パーティション生成修正

--- mysql/table.go
+++ mysql/table.go
@@ -191,9 +191,9 @@ func (p *IPartition) Create(table string) string {
 	for _, item := range p.Keys {
 		switch p.Type {
 		case "RANGE":
-			parts = append(append, fmt.Sprintf("\tPARTITION %s VALUES LESS THAN (%s)", item.Key, item.Value))
+			parts = append(parts, fmt.Sprintf("\tPARTITION %s VALUES LESS THAN (%s)", item.Key, item.Value))
 		case "LIST":
-			parts = append(append, fmt.Sprintf("\tPARTITION %s VALUES IN (%s)", item.Key, item.Value))
+			parts = append(parts, fmt.Sprintf("\tPARTITION %s VALUES IN (%s)", item.Key, item.Value))
 		}
 	}
 	buf.WriteString(strings.Join(parts, ",\n"))
